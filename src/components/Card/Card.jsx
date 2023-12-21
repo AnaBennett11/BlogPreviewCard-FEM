68b5f70dc6a055491285810b6a4daa7adad1a57c
Ana Bennett <anabennett1111@gmail.com>
2023-12-21 14:38:50 -0600

    Styled elements according to the figma file provided

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,17 +6,21 @@ import avatar from "../../../public/images/avatar.webp";
 
 const Card = () => {
   return (
-    <div className="card">
-      <img src={illustration}></img>
-      <button>Learning</button>
-      <p>Published 18 Dec 2023</p>
-      <h3>HTML & CSS foundations</h3>
-      <p>
-        These fundamental technologies serve as the backbone of every website,
-        defining its structure, content, and presentation.
-      </p>
-      <img src={avatar}></img>
-      <p>Alexander Frost</p>
+    <div className="cardContainer">
+      <div className="card">
+        <img src={illustration}></img>
+        <button>Learning</button>
+        <p>Published 18 Dec 2023</p>
+        <h3>HTML & CSS foundations</h3>
+        <p>
+          These fundamental technologies serve as the backbone of every website,
+          defining its structure, content, and presentation.
+        </p>
+        <div className="nameContainer">
+          <img src={avatar}></img>
+          <p>Alexander Frost</p>
+        </div>
+      </div>
     </div>
   );
 };
